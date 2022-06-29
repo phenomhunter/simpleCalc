2976130cb19d9255b01177047ee1ade49fcdbcd9
Orlando Mercado Tellado <67935991+phenomhunter@users.noreply.github.com>
2022-06-28 22:50:52 -0400

    Functionality Added
    Sum, Mult and Div functions

--- calc.js
+++ calc.js
@@ -1,5 +1,23 @@
-let inpLog = document.getElementById("log")
+let displayIn = document.getElementById("log");
 
-function calc() {
-    
+
+function mult() {
+    let num1 = document.getElementById("num1").value
+    let num2 = document.getElementById("num2").value
+    let result = num1 * num2
+    displayIn.innerText = result
+}
+
+function sum() {
+    let num1 = document.getElementById("num1").value
+    let num2 = document.getElementById("num2").value
+    let result = parseInt(num1) + parseInt(num2)
+    displayIn.innerText = result
+}
+
+function div() {
+    let num1 = document.getElementById("num1").value
+    let num2 = document.getElementById("num2").value
+    let result = num1 / num2
+    displayIn.innerText = result
 }
